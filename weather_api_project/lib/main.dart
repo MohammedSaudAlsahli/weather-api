195bd1b4e1e89b030fd72dcb296f68e671ed324c
Wejdan Alotaibi <85060152+wejdanturki7@users.noreply.github.com>
2023-09-03 02:03:50 +0300

    Update main.dart

--- weather_api_project/lib/main.dart
+++ weather_api_project/lib/main.dart
@@ -17,7 +17,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
+
         home: BlocProvider(
           create: (context) => DatabaseBloc(),
           child: const HomeScreen(),
